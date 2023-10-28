2d7ccf8811e9b789a395565eebd68146ebec249a
levantiendat <levan040669@gmail.com>
2023-10-28 17:28:05 +0700

    Initial commit

---
+++ RDC_ADMIN/src/AES.java
@@ -0,0 +1,76 @@
+import java.security.NoSuchAlgorithmException;
+import java.util.Base64;
+
+import javax.crypto.Cipher;
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.GCMParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
+
+public class AES {
+
+    private SecretKey key;
+    private final int KEY_SIZE = 128;
+    private final int T_LEN = 128;
+
+    public AES() throws Exception {
+
+        KeyGenerator generator = KeyGenerator.getInstance("AES");
+        generator.init(KEY_SIZE);
+        key = generator.generateKey();
+
+    }
+
+    public AES(String secretKeyStr) throws Exception {
+
+        key = new SecretKeySpec(decode(secretKeyStr),"AES");
+
+    }
+
+    public byte[] generateIV() throws Exception {
+
+        Cipher encryptionCipher = Cipher.getInstance("AES/GCM/NoPadding");
+        encryptionCipher.init(Cipher.ENCRYPT_MODE, key);
+        return encryptionCipher.getIV();
+
+    }
+
+    public String encrypt(String message, byte[] IV) throws Exception {
+        byte[] messageInBytes = message.getBytes();
+        Cipher encryptionCipher = Cipher.getInstance("AES/GCM/NoPadding");
+        GCMParameterSpec spec = new GCMParameterSpec(T_LEN, IV);
+        encryptionCipher.init(Cipher.ENCRYPT_MODE, key, spec);
+        byte[] encryptedBytes = encryptionCipher.doFinal(messageInBytes);
+        return encode(encryptedBytes);
+    }
+
+    public String decrypt(String encryptedMessage, byte[] IV) throws Exception {
+        byte[] messageInBytes = decode(encryptedMessage);
+        Cipher decryptionCipher = Cipher.getInstance("AES/GCM/NoPadding");
+        GCMParameterSpec spec = new GCMParameterSpec(T_LEN, IV);
+        decryptionCipher.init(Cipher.DECRYPT_MODE, key, spec);
+        byte[] decryptedBytes = decryptionCipher.doFinal(messageInBytes);
+        return new String(decryptedBytes);
+    }
+
+    private static String encode(byte[] data) {
+        return Base64.getEncoder().encodeToString(data);
+    }
+
+    private static byte[] decode(String data) {
+        return Base64.getDecoder().decode(data);
+    }
+
+    public String getKeyStr() {
+        return encode(key.getEncoded());
+    }
+
+    public static String getIVStr(byte[] IV) {
+        return encode(IV);
+    }
+
+    public static byte[] getIVFromStr(String IVStr) {
+        return decode(IVStr);
+    }
+
+}
---
+++ RDC_ADMIN/src/ClientAdmin.java
@@ -0,0 +1,158 @@
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.FileReader;
+import java.net.Socket;
+import java.security.PublicKey;
+import java.security.spec.ECField;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class ClientAdmin {
+
+    private static final String SERVER_INFO_PATH = "ServerInfo.txt";
+    private static final String COMP_ID_PATH = "CompID.txt";
+    private static final String KEY_PATH = "Key.txt";
+
+    String serverIP;
+    Integer serverPort;
+    PublicKey serverPublicKey;
+    String compID;
+    RSA rsa;
+    AES aes;
+    DataInputStream inp;
+    DataOutputStream out;
+    volatile Boolean isRunning = false;
+
+    public static void main(String[] args) {
+
+        ClientAdmin client = new ClientAdmin();
+        try {
+
+            client.Init();
+            client.Connect();
+            client.Interact();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("Error!");
+            client.Shutdown();
+        }
+
+    }
+
+    public void Init() throws Exception {
+
+        BufferedReader bf = new BufferedReader(new FileReader(COMP_ID_PATH));
+        compID = bf.readLine();
+        bf.close();
+
+        rsa = new RSA(KEY_PATH);
+
+        bf = new BufferedReader(new FileReader(SERVER_INFO_PATH));
+        serverIP = bf.readLine();
+        serverPort = Integer.valueOf(bf.readLine());
+        String publicKeyStr = bf.readLine();
+        serverPublicKey = RSA.getPublicKeyFromStr(publicKeyStr);
+        bf.close();
+
+        Socket socket = new Socket(serverIP, serverPort);
+        inp = new DataInputStream(socket.getInputStream());
+        out = new DataOutputStream(socket.getOutputStream());
+
+        System.out.println("Inited client");
+
+    }
+
+    public void Connect() throws Exception {
+
+        out.writeUTF(compID);
+
+        // client verify server
+        String testMes = String.valueOf((long)(Math.random() * 1e18));
+        String crypMes = RSA.encrypt(testMes, serverPublicKey);
+        out.writeUTF(crypMes);
+        String signMes = inp.readUTF();
+        if (!RSA.verify(testMes, signMes, serverPublicKey))
+            throw new Exception();
+
+        // server verify client
+        crypMes = inp.readUTF();
+        testMes = rsa.decrypt(crypMes);
+        signMes = rsa.sign(testMes);
+        out.writeUTF(signMes);
+
+        // get share key
+        String crypKey = inp.readUTF();
+        String key = rsa.decrypt(crypKey);
+        aes = new AES(key);
+
+        System.out.println("Connected to server!");
+
+    }
+
+    public void Interact() throws Exception {
+
+//        isRunning = true;
+//        while (isRunning) {
+//
+//            // handler func..
+//
+//        }
+
+        String option = "/OnlineList";
+        writeMes(option);
+        int n = Integer.parseInt(readMes());
+        System.out.println(n + "");
+        List<String> onlineComps = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            String onlineComp = readMes();
+            onlineComps.add(onlineComp);
+            System.out.println(onlineComp);
+        }
+
+        String option1 = "/AppHistory";
+        writeMes(option1);
+        writeMes("employee1");
+        int n1 = Integer.parseInt(readMes());
+        System.out.println(n1);
+        List<List<String>> apps = new ArrayList<>();
+        for(int i = 0; i < n1; i++){
+            String appName = readMes();
+            String timeID = readMes();
+            apps.add(Arrays.asList(appName, timeID));
+            System.out.println(i+". "+appName+", "+timeID);
+        }
+        RemoteControlView view = new RemoteControlView("Remote control", onlineComps,apps);
+
+    }
+
+
+    public void Shutdown() {
+
+        isRunning = false;
+        System.out.println("Disconnect to server..");
+
+    }
+
+    public String readMes() throws Exception {
+
+        String IVStr = inp.readUTF();
+        String crypMes = inp.readUTF();
+        byte[] IV = AES.getIVFromStr(IVStr);
+        return aes.decrypt(crypMes, IV);
+
+    }
+
+    public void writeMes(String mes) throws Exception {
+
+        byte[] IV = aes.generateIV();
+        String crypMes = aes.encrypt(mes, IV);
+        String IVStr = AES.getIVStr(IV);
+        out.writeUTF(IVStr);
+        out.writeUTF(crypMes);
+
+    }
+
+}
